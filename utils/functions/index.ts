24e4503a10082e25c3e1e640e95d59aa39484f3d
kimura keisuke <mail@kkeisuke.com>
2023-11-13 00:02:07 +0900

    日付フォーマット修正

--- utils/functions/index.ts
+++ utils/functions/index.ts
@@ -4,5 +4,5 @@ export const formatDateTime = (dateString: string): string => {
   if (Number.isNaN(date.getTime())) {
     return dateString
   }
-  return new Intl.DateTimeFormat('ja', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', timeZone: 'UTC' }).format(date)
+  return new Intl.DateTimeFormat('ja', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(date)
 }
