7a6324f946bd4aa3e24260355dd1d4a87ddcd625
kimura keisuke <mail@kkeisuke.com>
2023-11-13 00:35:55 +0900

    入力用の日付フォーマット追加

--- utils/functions/index.ts
+++ utils/functions/index.ts
@@ -6,3 +6,12 @@ export const formatDateTime = (dateString: string): string => {
   }
   return new Intl.DateTimeFormat('ja', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(date)
 }
+
+export const formatDateTimeForInput = (dateString: string): string => {
+  const date = new Date(dateString)
+  // Invalid Date
+  if (Number.isNaN(date.getTime())) {
+    return dateString
+  }
+  return new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000).toISOString().slice(0, 16)
+}
