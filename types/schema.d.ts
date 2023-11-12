3b09ced0575dee9bb74cde3f15a2060d144dfc30
kimura keisuke <mail@kkeisuke.com>
2023-11-12 18:22:18 +0900

    supabase gen types typescript
    https://supabase.com/docs/reference/cli/supabase-gen-types-typescript

---
+++ types/schema.d.ts
@@ -0,0 +1,57 @@
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      todos: {
+        Row: {
+          completed_at: string | null
+          created_at: string
+          deadline: string
+          id: string
+          memo: string
+          title: string
+          updated_at: string
+          user_id: string
+        }
+        Insert: {
+          completed_at?: string | null
+          created_at?: string
+          deadline?: string
+          id?: string
+          memo?: string
+          title?: string
+          updated_at?: string
+          user_id?: string
+        }
+        Update: {
+          completed_at?: string | null
+          created_at?: string
+          deadline?: string
+          id?: string
+          memo?: string
+          title?: string
+          updated_at?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+export type TodoAdd = Omit<Database['public']['Tables']['todos']['Insert'], 'id', 'user_id'>
+export type TodoUpdate = Omit<Database['public']['Tables']['todos']['Update'], 'id', 'user_id'>
+export type Todo = Omit<Database['public']['Tables']['todos']['Row'], 'user_id'>
