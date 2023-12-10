30d4f1639cf1f7b714109b2ab5d5228ee22119d7
kimura keisuke <mail@kkeisuke.com>
2023-12-11 00:55:01 +0900

    layout 追加

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="deafult-layout">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.deafult-layout {
+  margin: 0 auto;
+  padding-block: 1rem;
+  max-width: min(30rem, calc(100% - 2rem));
+  min-height: 100vh;
+  box-sizing: border-box;
+}
+</style>
