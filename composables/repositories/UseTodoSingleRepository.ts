495be7db40a47a35e456ba26e08ce0bd4349ec9f
kimura keisuke <mail@kkeisuke.com>
2023-12-17 23:29:27 +0900

    UTC 変換漏れ

--- composables/repositories/UseTodoSingleRepository.ts
+++ composables/repositories/UseTodoSingleRepository.ts
@@ -8,6 +8,9 @@ export const useTodoSingleRepository = () => {
       return todos.select('id, title, memo, deadline, completed_at, updated_at, created_at').eq('id', id).single()
     },
     updateTodo(id: Todo['id'], todo: TodoUpdate) {
+      if (todo.deadline) {
+        todo.deadline = new Date(todo.deadline).toISOString()
+      }
       return todos.update({ ...todo, updated_at: new Date().toISOString() }).eq('id', id)
     },
     completeTodo(id: Todo['id']) {
