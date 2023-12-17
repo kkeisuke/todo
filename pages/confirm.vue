74914216a24cc41e7be684a6243da4d55a2ba167
kimura keisuke <mail@kkeisuke.com>
2023-12-17 13:32:35 +0900

    ログイン確認ページ追加

---
+++ pages/confirm.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+watch(
+  user,
+  () => {
+    if (user.value) {
+      return navigateTo('/')
+    }
+  },
+  { immediate: true }
+)
+</script>
+
+<template>
+  <nuxt-layout>
+    <main class="main">
+      <todo-header />
+      <div>Waiting for login...</div>
+    </main>
+  </nuxt-layout>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+</style>
