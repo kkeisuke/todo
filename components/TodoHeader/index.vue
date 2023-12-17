b788ab2b3626c62ae7e1dbe7fa99251206382b99
kimura keisuke <mail@kkeisuke.com>
2023-12-17 12:04:29 +0900

    ヘッダーコンポーネント追加

---
+++ components/TodoHeader/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+const { logout } = useAuth()
+
+const handleLogout = async () => {
+  await logout()
+  navigateTo('/login')
+}
+</script>
+
+<template>
+  <header class="header">
+    <div class="logo">
+      <nuxt-link to="/">Todo</nuxt-link>
+    </div>
+    <ul v-if="user" class="user-info">
+      <li>{{ user.email }}</li>
+      <li><button type="button" @click="handleLogout">logout</button></li>
+    </ul>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: grid;
+  gap: 16px;
+}
+.logo {
+  margin: 0;
+  font-size: 2rem;
+  font-weight: bold;
+  & > a {
+    text-decoration: none;
+    color: inherit;
+  }
+}
+.user-info {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+}
+</style>
