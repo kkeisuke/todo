41c1e2d9c80568b6ea38811c77c5d782e619feaf
kimura keisuke <mail@kkeisuke.com>
2023-12-23 18:01:10 +0900

    Todo 一覧メニューのコンポーネント追加

---
+++ components/TodoList/TodoListMenu.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'TodoListMenu'
+})
+
+const router = useRouter()
+const { todos, params } = injectUseTodoCollection()
+
+const handleShowCompleted = () => {
+  router.push({ query: { completed: params.showCompleted ? '1' : undefined } })
+}
+</script>
+
+<template>
+  <section class="menu">
+    <label><input v-model="params.showCompleted" type="checkbox" @change="handleShowCompleted" /> 完了済みを表示</label>
+    <span>{{ todos.length }} 件</span>
+  </section>
+</template>
+
+<style scoped>
+.menu {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
