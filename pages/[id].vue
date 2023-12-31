cbe242a9878d7257f30b331b75e2fb2b4d823b55
kimura keisuke <mail@kkeisuke.com>
2023-12-31 13:18:33 +0900

    ページコンポーネント追加

---
+++ pages/[id].vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const route = useRoute()
+if (typeof route.params.id !== 'string') {
+  throw new Error('invalid id')
+}
+const { todo } = provideUseTodoSingle(route.params.id)
+</script>
+
+<template>
+  <nuxt-layout>
+    <main class="main">
+      <todo-header />
+      <todo-detail v-if="todo" :todo="todo" />
+      <div v-else>not found</div>
+    </main>
+  </nuxt-layout>
+</template>
+
+<style scoped>
+.main {
+  display: grid;
+  gap: 3rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+user.value?.id && provideUseTodoCollection(user.value.id)
+</script>
+
+<template>
+  <nuxt-layout>
+    <main class="main">
+      <todo-header />
+      <todo-add />
+      <todo-list-menu />
+      <todo-list />
+    </main>
+  </nuxt-layout>
+</template>
+
+<style scoped>
+.main {
+  display: grid;
+  gap: 1.5rem;
+}
+</style>
