19f9c3f9e5db9f49a9d3f82da7256217039703d2
kimura keisuke <mail@kkeisuke.com>
2024-01-27 11:54:07 +0900

    登録後にリセットするように修正

--- composables/store/UseTodoAdd.ts
+++ composables/store/UseTodoAdd.ts
@@ -3,16 +3,19 @@ import type { TodoAdd } from '~/types/schema'
 export const useTodoAdd = () => {
   const { addTodo } = useTodoCollectionRepository()
 
-  const data = reactive<TodoAdd>({
+  const getDefaultData = (): TodoAdd => ({
     title: '',
     memo: '',
     deadline: formatDateTimeForInput(new Date().toString())
   })
 
+  const data = reactive<TodoAdd>(getDefaultData())
+
   return {
     data,
     async addTodo() {
       const res = await addTodo(data)
+      Object.assign(data, getDefaultData())
       return res.count
     }
   }
